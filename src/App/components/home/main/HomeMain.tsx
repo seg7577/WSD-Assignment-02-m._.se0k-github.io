2fd30a914710d432a6fc34f7cfa0566c32e5688c
seg7577 <seg082911@gmail.com>
2024-11-13 21:38:06 +0900

    테스트 병합 커밋

--- src/App/components/home/main/HomeMain.tsx
+++ src/App/components/home/main/HomeMain.tsx
@@ -4,7 +4,6 @@ import Banner from '../../banner/Banner';
 import MovieRow from '../../movie-row/MovieRow';
 import './HomeMain.css'; // 스타일 가져오기
 
-//homeMain branch create
 const HomeMain = () => {
   // 배너에 표시할 대표 영화 데이터
   const featuredMovie = {
@@ -38,30 +37,38 @@ const HomeMain = () => {
 
   return (
     <div className="home-main">
+      {/* 홈 메인 컴포넌트를 감싸는 컨테이너 */}
+      
       {/* 배너 컴포넌트 */}
+      {/* 주요 영화(Featured Movie) 정보를 표시하는 배너 */}
       <Banner movie={featuredMovie} />
-
+  
       {/* 영화 슬라이더 컴포넌트 */}
+      {/* 인기 영화 슬라이더 */}
       <MovieRow
-        title="인기 영화"
-        fetchUrl={popularMoviesUrl}
-        getImageUrl={getImageUrl}
-        toggleWishlist={toggleWishlist}
-        isInWishlist={isInWishlist}
+        title="인기 영화"                // 섹션 제목: '인기 영화'
+        fetchUrl={popularMoviesUrl}     // 인기 영화를 가져오는 API URL
+        getImageUrl={getImageUrl}       // 영화 이미지 URL을 생성하는 함수
+        toggleWishlist={toggleWishlist} // 위시리스트에 추가/제거하는 함수
+        isInWishlist={isInWishlist}     // 영화가 위시리스트에 있는지 확인하는 함수
       />
+  
+      {/* 최신 영화 슬라이더 */}
       <MovieRow
-        title="최신 영화"
-        fetchUrl={newReleasesUrl}
-        getImageUrl={getImageUrl}
-        toggleWishlist={toggleWishlist}
-        isInWishlist={isInWishlist}
+        title="최신 영화"                // 섹션 제목: '최신 영화'
+        fetchUrl={newReleasesUrl}       // 최신 영화를 가져오는 API URL
+        getImageUrl={getImageUrl}       // 영화 이미지 URL을 생성하는 함수
+        toggleWishlist={toggleWishlist} // 위시리스트에 추가/제거하는 함수
+        isInWishlist={isInWishlist}     // 영화가 위시리스트에 있는지 확인하는 함수
       />
+  
+      {/* 액션 영화 슬라이더 */}
       <MovieRow
-        title="액션 영화"
-        fetchUrl={actionMoviesUrl}
-        getImageUrl={getImageUrl}
-        toggleWishlist={toggleWishlist}
-        isInWishlist={isInWishlist}
+        title="액션 영화"                // 섹션 제목: '액션 영화'
+        fetchUrl={actionMoviesUrl}      // 액션 영화를 가져오는 API URL
+        getImageUrl={getImageUrl}       // 영화 이미지 URL을 생성하는 함수
+        toggleWishlist={toggleWishlist} // 위시리스트에 추가/제거하는 함수
+        isInWishlist={isInWishlist}     // 영화가 위시리스트에 있는지 확인하는 함수
       />
     </div>
   );
